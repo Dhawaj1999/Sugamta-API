ee2006bb6395e5959214b96ac2b3fe2d2cb28f4d
DotNetDeveloper12 <sarojsuraj714@gmail.com>
2023-12-20 19:43:35 +0530

    test

--- Sugamta.API/Models/obj/Debug/net8.0/Models.AssemblyInfo.cs
+++ Sugamta.API/Models/obj/Debug/net8.0/Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dca6c23719b4480a29b724b5eecb17306748a1b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
