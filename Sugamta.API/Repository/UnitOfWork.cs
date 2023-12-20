07adfae2654b64c5ea19635176a9dfe327d3492e
DotNetDeveloper12 <sarojsuraj714@gmail.com>
2023-12-20 18:58:18 +0530

    suraj_api

---
+++ Sugamta.API/Repository/UnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using DataAccessLayer.DAL;
+using Microsoft.EntityFrameworkCore;
+using Models.Models;
+using Sugamta.API.Repository.Interface;
+
+namespace Sugamta.API.Repository
+{
+    public class UnitOfWork:IUnitOfWork
+    {
+        private readonly UserDbContext _context;
+        public IUser user { get; private set; }
+        public UnitOfWork(UserDbContext context)
+        {
+
+            _context = context;
+            user = new UserRepo(_context);
+
+        }
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+    }
+}
